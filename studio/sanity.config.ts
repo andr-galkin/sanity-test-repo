888724323a3693effeadb52333780c0d7e917ba4
Andrii Galkin <andriigalkin@MacBook-von-Andrii.local>
2023-05-09 10:48:46 +0200

    update

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import { dashboardTool } from "@sanity/dashboard";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,26 @@ export default defineConfig({
   projectId: 'j3fd117b',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Portfolio-web',
+              apiId: 'e2a6be71-cfd2-4031-bdc2-8bf4ac681566',
+              buildHookId: 'https://api.netlify.com/build_hooks/645a088fda3094007163fba5',
+              name: 'chipper-starburst-9a19a7',
+              url: 'chipper-starburst-9a19a7.netlify.app',
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
